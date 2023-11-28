bbcdfcaa759055367fa8a61408e3ff68dff38332
Michael Zöller <michael2.zoeller@gmail.com>
2023-11-28 11:04:30 +0100

    Fix compile

--- 03b_netzfilm/src/main/java/de/zettsystems/netzfilm/NetzfilmApplication.java
+++ 03b_netzfilm/src/main/java/de/zettsystems/netzfilm/NetzfilmApplication.java
@@ -1,12 +1,16 @@
 package de.zettsystems.netzfilm;
 
+import de.zettsystems.timeutil.autoconfigure.TimetravelProperties;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
 public class NetzfilmApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(NetzfilmApplication.class, args);
     }
--- 04_netzfilm/src/main/java/de/zettsystems/netzfilm/movie/values/NoSuchCopyException.java
+++ 04_netzfilm/src/main/java/de/zettsystems/netzfilm/movie/values/NoSuchCopyException.java
@@ -6,6 +6,6 @@ import java.util.UUID;
 
 public class NoSuchCopyException extends RuntimeException {
     public NoSuchCopyException(UUID uuid) {
-        super(HttpStatus.NOT_FOUND, "No copy exists with uuid " + uuid);
+        super("No copy exists with uuid " + uuid);
     }
 }
