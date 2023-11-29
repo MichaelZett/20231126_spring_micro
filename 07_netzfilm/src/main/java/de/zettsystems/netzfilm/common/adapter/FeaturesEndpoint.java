915af5a8c2bb8d0cc09e44264f3e56d8c6aab0f7
Michael Zöller <michael2.zoeller@gmail.com>
2023-11-29 11:10:31 +0100

    Add missing actuator stuff

---
+++ 07_netzfilm/src/main/java/de/zettsystems/netzfilm/common/adapter/FeaturesEndpoint.java
@@ -0,0 +1,39 @@
+package de.zettsystems.netzfilm.common.adapter;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "features")
+public class FeaturesEndpoint {
+
+    private Map<String, Feature> features = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void setup() {
+        Feature feature = new Feature("Some nice feature", Boolean.TRUE);
+        features.put(feature.name, feature);
+        feature = new Feature("Feature in development", Boolean.FALSE);
+        features.put(feature.name, feature);
+    }
+
+    @ReadOperation
+    public Map<String, Feature> features() {
+        return features;
+    }
+
+    @ReadOperation
+    public Feature feature(@Selector String name) {
+        return features.get(name);
+    }
+
+    public record Feature(String name, Boolean enabled) {
+    }
+
+}
